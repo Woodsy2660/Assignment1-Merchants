7210bde0f8f3f2cdd56286fa2012c421e1708a48
Leo-Carlton <160807341+Leo-Carlton@users.noreply.github.com>
2025-09-03 16:01:17 +1000

    Update activate_LED.v to turn off if mole_hit is ture

--- Switch\Rng_modules/activate_LED.v
+++ Switch\Rng_modules/activate_LED.v
@@ -1,25 +1,36 @@
-module activate_LED (
+module mole_controller (
     input        clk,
     input        rst,
     input        LED_toggle,       // from timer (one pulse per cycle)
-    input  [4:0] random_value,     // from RNG (0–17)
+    input  [4:0] rng_led,          // from RNG (0–17)
+    input        hit_pulse,        // 1-cycle pulse from mole_detector
     output reg [17:0] LEDR         // drives LEDs
 );
 
     reg [4:0] current_led;  // which LED is active
+    reg       mole_active;  // keeps track if LED is on
 
     always @(posedge clk or posedge rst) begin
         if (rst) begin
             current_led <= 5'd0;
-        end 
-        else if (LED_toggle) begin
-            current_led <= rng_led;   // pick new random LED only on toggle
+            mole_active <= 1'b0;
+        end else begin
+            // Turn on new mole on LED_toggle
+            if (LED_toggle) begin
+                current_led <= rng_led;
+                mole_active <= 1'b1;
+            end
+
+            // Turn off LED if mole was hit
+            if (hit_pulse && mole_active)
+                mole_active <= 1'b0;
         end
     end
 
     always @(*) begin
         LEDR = 18'b0;
-        LEDR[current_led] = 1'b1;     // turn on current LED
+        if (mole_active)
+            LEDR[current_led] = 1'b1;
     end
 
 endmodule
